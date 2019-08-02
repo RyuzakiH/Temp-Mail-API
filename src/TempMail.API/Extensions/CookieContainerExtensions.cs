dd8c2d56c3e7f17f275b70d5ed7965831ad13140
RyuzakiH <hazemahmedamr.zero@gmail.com>
2019-08-02 02:51:01 +0200

    Fixed Delete function by updating CookieExtensions

--- src/TempMail.API/Extensions/CookieContainerExtensions.cs
+++ src/TempMail.API/Extensions/CookieContainerExtensions.cs
@@ -9,20 +9,34 @@ namespace TempMail.API.Extensions
             cookieContainer.GetCookies(uri)[cookieName];
 
         internal static Cookie GetCookie(this CookieContainer cookieContainer, string uri, string cookieName) =>
-            cookieContainer.GetCookies(new Uri(uri))[cookieName];
+            cookieContainer.GetCookie(new Uri(uri), cookieName);
 
 
-        internal static void SetCookie(this CookieContainer cookieContainer, Uri uri, string cookieHeader) =>
+        internal static void AddCookie(this CookieContainer cookieContainer, Uri uri, string cookieHeader) =>
             cookieContainer.SetCookies(uri, cookieHeader);
 
-        internal static void SetCookie(this CookieContainer cookieContainer, string uri, string cookieHeader) =>
-            cookieContainer.SetCookies(new Uri(uri), cookieHeader);
+        internal static void AddCookie(this CookieContainer cookieContainer, string uri, string cookieHeader) =>
+            cookieContainer.AddCookie(new Uri(uri), cookieHeader);
 
 
-        internal static void SetCookie(this CookieContainer cookieContainer, Uri uri, string cookieName, string cookieValue) =>
+        internal static void AddCookie(this CookieContainer cookieContainer, Uri uri, string cookieName, string cookieValue) =>
             cookieContainer.SetCookies(uri, $"{cookieName}={cookieValue}");
 
+        internal static void AddCookie(this CookieContainer cookieContainer, string uri, string cookieName, string cookieValue) =>
+            cookieContainer.AddCookie(new Uri(uri), cookieName, cookieValue);
+
+
+        internal static void SetCookie(this CookieContainer cookieContainer, Uri uri, string cookieName, string cookieValue)
+        {
+            var cookie = cookieContainer.GetCookie(uri, cookieName);
+            
+            if (cookie != null)
+                cookie.Value = cookieValue;
+            else
+                cookieContainer.AddCookie(uri, $"{cookieName}={cookieValue}");
+        }
+
         internal static void SetCookie(this CookieContainer cookieContainer, string uri, string cookieName, string cookieValue) =>
-            cookieContainer.SetCookies(new Uri(uri), $"{cookieName}={cookieValue}");
+            cookieContainer.SetCookie(new Uri(uri), cookieName, cookieValue);
     }
 }
